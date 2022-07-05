9092417465c16318cfe48823387418fa000f3b9c
Tsegazeab <Tsegazeabwuta@gmail.com>
2022-07-05 06:49:14 -0700

    a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints _putchar
+ *
+ * Return: alwaays(0) success
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
