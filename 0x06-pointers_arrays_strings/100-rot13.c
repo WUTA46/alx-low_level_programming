efd88827ab1ccb2b8ebdf5ae2dc9389fef279beb
Tsegazeab <Tsegazeabwuta@gmail.com>
2022-07-15 03:13:34 -0700

    a function that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encode string using rot13
+ * @c: input string
+ * Return: c
+ */
+
+char *rot13(char *c)
+{
+	int i, j;
+	char alph[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ralph[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; alph[j] != '\0'; j++)
+		{
+			if (c[i] == alph[j])
+			{
+				c[i] = ralph[j];
+			}
+		}
+	}
+
+	return (c);
+}
+
